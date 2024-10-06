15462bfecb8a974b0e5884f5bee22a9254485ae2
WiyandaSavitri <wiyandasavitri@gmail.com>
2024-10-06 23:16:14 +0700

    Update programtebakAngka.cpp
    Program yang telah dibuat bernama "Game Tebak Angka" dimana game ini bertujuan untuk menebak angka yang dipilih secara acak oleh komputer dari rentang 1 sampai 25. Pemain harus mencoba menebak angka tersebut. Jika tebakan terlalu kecil, komputer akan memberi tahu bahwa tebakan "Salah" dan "terlalu kecil." Sebaliknya, jika tebakan terlalu besar, komputer akan memberi tahu bahwa tebakan "Salah" dan "Terlalu besar."
    Dan akan terus berlanjut hingga pemain berhasil menebak angka yang benar.

--- programtebakAngka.cpp
+++ programtebakAngka.cpp
@@ -1,37 +1,48 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int getUserGuess() {
+    int angka;
+    cout << "Saya  : Angkanya adalah = ";
+    cin >> angka;
+
     
-   int angka, t, n = 0;
-    
-    // angka = Angka yang ditebak user
-    // t     = Angka Pilihan Computer
-    // n     = Berapa Kali User Menebak
-    
-    srand (time(NULL));
-    t = rand() % 10 + 1;
-    cout << "\t Program Tebak Angka 1-25 \n\n";
-    
-    cout << "Computer : Saya Memikirkan sebuah Angka 1-25\n";
-    cout << "Computer : Angka Apakah itu ? \n\n";
-    
-    do{
-        n++;
+    while (angka < 1 || angka > 25) {
+        cout << "\n\t SALAH ! Masukkan angka antara 1 dan 25.\n";
         cout << "Saya  : Angkanya adalah = ";
-        cin >> angka ;
+        cin >> angka;
+    }
+    
+    return angka;
+}
+
+
+void playGame() {
+    int angka, t, n = 0;
+    
+    
+    t = 1 + rand() % 25; 
+
+    cout << "\t Program Tebak Angka 1-25 \n\n";
+    cout << "Computer: Saya Memikirkan sebuah Angka 1-25\n";
+    cout << "Computer: Angka Apakah itu? \n\n";
+
+    do {
+        n++;
+        angka = getUserGuess();
         
-        if (angka < t){
-            
+        if (angka < t) {
             cout << "\n\t SALAH ! Angkanya Terlalu Kecil \n\n";
-        }
-        else if (angka > t){
-            
+        } else if (angka > t) {
             cout << "\n\t SALAH ! Angka Terlalu Besar \n\n";
         }
     } while (angka != t);
     
-    cout << "\n\t BENAR ! Anda Telah Menebak Sebanyak "<< n <<" Kali"; 
+    cout << "\n\t BENAR ! Anda Telah Menebak Sebanyak " << n << " Kali\n"; 
+}
 
+int main() {
+    srand(time(0)); 
+    playGame(); 
     return 0;
 }
