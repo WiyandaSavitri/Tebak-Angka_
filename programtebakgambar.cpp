462dbf6260457adc67a2a132ab027d89f98b83dc
WiyandaSavitri <wiyandasavitri@gmail.com>
2024-10-06 19:36:42 +0700

    Create programtebakgambar.cpp
    Program yang kami buat adalah game sederhana bernama "Tebak Angka". Game ini dirancang untuk membantu pemain berlatih logika dan berpikir kritis. Dalam permainan ini, cara yang digunakan adalah komputer secara acak memilih sebuah angka antara 1 sampai 25 dan nantinya pemain diminta untuk menebak angka tersebut.

---
+++ programtebakgambar.cpp
@@ -0,0 +1,38 @@
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+   int angka, t, n = 0;
+    
+    // angka = Angka yang ditebak user
+    // t     = Angka Pilihan Computer
+    // n     = Berapa Kali User Menebak
+    
+    srand (time(NULL));
+    t = rand() % 10 + 1;
+    cout << "\t Program Tebak Angka 1-25 \n\n";
+    
+    cout << "Computer : Saya Memikirkan sebuah Angka 1-25\n";
+    cout << "Computer : Angka Apakah itu ? \n\n";
+    
+    do{
+        n++;
+        cout << "Saya  : Angkanya adalah = ";
+        cin >> angka ;
+        
+        if (angka < t){
+            
+            cout << "\n\t SALAH ! Angkanya Terlalu Kecil \n\n";
+        }
+        else if (angka > t){
+            
+            cout << "\n\t SALAH ! Angka Terlalu Besar \n\n";
+        }
+    } while (angka != t);
+    
+    cout << "\n\t BENAR ! Anda Telah Menebak Sebanyak "<< n <<" Kali"; 
+
+    return 0;
+}
